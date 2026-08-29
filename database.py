7bb0df47f5c90367c5770008fb25514dfae5291e
Pranav Thakor <pranavthakor2006@gmail.com>
2026-08-30 00:19:56 +0530

    intial changes

---
+++ database.py
@@ -0,0 +1,13 @@
+import psycopg
+import os
+from dotenv import load_dotenv
+
+conn = psycopg.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),        
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")    
+)
+
+print("Database Connected!")
