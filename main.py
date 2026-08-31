35ed2998651d3034ca5ddf2b9175cef6216597f0
Pranav Thakor <pranavthakor2006@gmail.com>
2026-08-31 21:35:37 +0530

    add a post users

--- main.py
+++ main.py
@@ -11,4 +11,30 @@ def home():
     return {"message":"Appointment api is running!"}
 
 
+class UserCreate(BaseModel):
+    name : str
+    email : str
+    password : str
+    role : str = "customer"
+    
+@app.post("/users")
+def create_user(user:UserCreate):
+     
+    cursor = conn.cursor()
+     
+    cursor.execute(
+         """insert into users (name,email,password_hash,role) values (%s,%s,%s,%s)""",
+         (user.name,user.email,user.password,user.role)
+     )
+     
+    conn.commit()
+     
+    cursor.close()
+     
+    return{
+            "message":"User created succesfully" ,
+        }
+     
+     
+    
 
